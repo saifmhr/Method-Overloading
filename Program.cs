1757d7717db684a5fbb9a5efe401b37e7d3e7d28
saifmhr <saifmahar165@gmail.com>
2022-10-21 22:42:29 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+
+public class Program
+    {
+     public  static void Main()
+        {
+        Add(20, 30);
+        Add(10, 20, 10);
+        }
+
+    public static void Add(int FN, int LN)
+    {
+        Console.WriteLine("Sum = {0} ", FN + LN);
+    }
+
+    public static void Add(int FN, int LN,int TN)
+    {
+        Console.WriteLine("Sum = {0} ", FN + LN + TN);
+    }
+
+}
